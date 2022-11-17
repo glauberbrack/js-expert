c42f136a0b6a0c9761dadd544d3eed8d859c832d
glauberbrack <glauber@brack.com.br>
2022-11-16 22:54:40 -0300

    class 01

---
+++ class-01-mocks/src/constants.js
@@ -0,0 +1,8 @@
+const constants = {
+  error: {
+    FILE_LENGTH_ERROR_MESSAGE: "The content length is invalid! 🥺",
+    FILE_FIELDS_ERROR_MESSAGE: "The provided properties are invalid! 🥺",
+  },
+};
+
+module.exports = constants;
